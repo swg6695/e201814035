c2b177917b6115c66424c11ffd18e836f2ca494b
Song, Wongeun <anysong132@gamil.com>
2024-04-24 13:04:29 +0900

    fifth

--- src/e201814035/Hello.java
+++ src/e201814035/Hello.java
@@ -1,7 +1,8 @@
 package e201814035;
 
 public class Hello {
-    public static void run() {
-        System.out.println("hello world");
-    }
+	public static void run() {
+		System.out.println("hello world");
+		System.out.println("송원근");
+	}
 }
